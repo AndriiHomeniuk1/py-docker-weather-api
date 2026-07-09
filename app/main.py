d6284ba898b1c0d5bc8d31e24483cc7f11cb1b6f
Andrii Homeniuk <andreygomenuk@gmail.com>
2026-07-09 12:41:28 +0300

    feat(main): implement basic weather fetch in main.py
    - Add get_weather function using requests and API_KEY from env
    - Fetch current weather for Paris from WeatherAPI
    - Parse location, local time, temperature, and condition
    - Print formatted weather information to console

--- app/main.py
+++ app/main.py
@@ -1,6 +1,29 @@
+import os
+import requests
+
+
+URL = "https://api.weatherapi.com/v1/current.json"
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    api_key = os.getenv("API_KEY")
+    params = {
+        "key": api_key,
+        "q": "Paris",
+        "aqi": "no"
+    }
+    response = requests.get(URL, params=params)
+    data = response.json()
+
+    name = data["location"]["tz_id"]
+    local_time = data["location"]["localtime"]
+    temperature_c = data["current"]["temp_c"]
+    condition = data["current"]["condition"]["text"]
+
+    print(
+        (f"{name} {local_time} "
+         f"Weather: {temperature_c} Celsius, {condition}")
+    )
 
 
 if __name__ == "__main__":
